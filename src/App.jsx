f3fb551a5dc3beef91f1ab6a5e3ae9c086aa1821
ThibautRingdev <thibautringeval@gmail.com>
2025-12-22 13:23:28 +0100

    Add examples of variables, functions and arrays in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import JSXrules from "./components/JSXRules"
 
 const App = () => {
-  return <div> 
+  const myName = "Thib Ring"
+  const multiply = (a, b) => a * b;
+  return (
+  <section>
+    <p>2 * 2 = {multiply(2,2)}</p>
+    <h1>Mon prénom est {myName}</h1>
+    <p>Mes amis sont {['Pierre', 'Jacques', 'Paul']}</p>
+  <div> 
     <JSXrules /> 
   </div>
+  </section>
+  )
 }
 
 export default App;
