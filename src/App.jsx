fc9d3f141ca649120cbb2225240a56d43e95de52
ThibautRingdev <thibautringeval@gmail.com>
2025-12-29 17:32:07 +0100

    add update and remove operations on array state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 
 const App = () => {
- const [count, setCount] = useState(0);
- const increment = () => setCount(count+1);
- const decrement = () => setCount(count-1);
+  const [friends, setFriends] = useState(['Alex', 'John'])
+
+  const addOneFriend = () => setFriends([...friends, "Thibaut"]);
+  const removeOneFriend = () => setFriends(friends.filter(f => f !== "Thibaut"));
+  const updateOneFriend = () => {
+    setFriends(friends.map(f => f === "Alex" ? "Alex Toucourt" : f));
+  }
 
  return <section>
-  <h1>{count}</h1>
-  <button onClick={increment}>Plus</button>
-  <button onClick={decrement}>Moins</button>
+  {friends.map(f => (
+    <li key={f}>{f}</li>
+  ))}
+  <button onClick={addOneFriend}>Ajouter un nouvel ami</button>
+  <button onClick={removeOneFriend}>Retirer un ami de cette liste</button>
+  <button onClick={updateOneFriend}>Actualiser la liste</button>
  </section>;
 
 };
