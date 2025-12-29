aac1b3291de52df9357d6d934d772d0037b08bf4
ThibautRingdev <thibautringeval@gmail.com>
2025-12-29 18:30:56 +0100

    lift state up and share state between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import { useState } from "react"
+import ComponentOne from "./components/useState/ComponentOne";
+import ComponentTwo from "./components/useState/ComponentTwo";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    {id: '1', title: "Spider-Man 1", ratings: 7},
-    {id: '2', title: "Spider-Man 2", ratings: 9},
-    {id: '3', title: "Spider-Man 3", ratings: 4}
-  ]);
-
-  const handleClick = () => {
-    setMovies(
-      movies.map(m => m.id === '1' ? {...m, title: "John Wick 4"} : m)
-    )
-  }
-
+  const [count,setCount] = useState(0)
   return <section>
-    {movies.map(m => (
-      <li key={m.id}>{m.title}</li>
-    ))}
-
-    <button onClick={handleClick}>Change le nom avec ce bouton</button>
-  </section>;
+<ComponentOne count={count} onClickHandler={() => setCount(count +1)}/>
+<ComponentTwo count={count} onClickHandler={() => setCount(count +1)}/>
+  </section>
 };
 
 export default App;
---
+++ src/components/useState/ComponentTwo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ComponentTwo({count, onClickHandler}) {
+  const handleClick = () => onClickHandler()
+  
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  )
+}
+
+export default ComponentTwo
