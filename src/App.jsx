df186c670c902543353a5a39274fdd1653bea31b
ThibautRingdev <thibautringeval@gmail.com>
2025-12-29 17:15:27 +0100

    add decrement constant

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 const App = () => {
  const [count, setCount] = useState(0);
- const increment = () => setCount(count+1)
+ const increment = () => setCount(count+1);
+ const decrement = () => setCount(count-1);
 
  return <section>
   <h1>{count}</h1>
   <button onClick={increment}>Plus</button>
+  <button onClick={decrement}>Moins</button>
  </section>;
 
 };
